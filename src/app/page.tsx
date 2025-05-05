c7584ec8acc4c5527012bd3209b2b78d9d5c7bc5
Ryan Figueredo <ryan.figueredo059@gmail.com>
2025-05-05 00:03:08 -0300

    Reorder component imports and render order for improved structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import Benefits from "@/components/Benefits";
+import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
+import Partners from "@/components/Partners";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Benefits />
+      <Partners />
+      <Testimonials />
+      <Contact />
     </div>
   );
 }
